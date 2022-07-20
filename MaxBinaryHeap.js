15d824b03936728e2a72e96d812f16fe681aba9a
HiiragiMatu <matuyou0301@gmail.com>
2022-07-20 21:23:08 +0800

    MaxBinaryHeap Extract

--- MaxBinaryHeap.js
+++ MaxBinaryHeap.js
@@ -1,6 +1,7 @@
 class MaxBinaryHeap {
     constructor() {
-        this.values = [41, 39, 33, 18, 27, 12];
+        // this.values = [41, 39, 33, 18, 27, 12];
+        this.values = [];
     }
 
     insert(value) {
@@ -23,13 +24,73 @@ class MaxBinaryHeap {
             idx = parentIdx;
         }
     }
+
+    sinkDown() {
+        let idx = 0;
+        const len = this.values.length;
+        const el = this.values[0];
+
+        while (true) {
+            let leftIdx = 2 * idx + 1;
+            let rightIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+
+            let swap = null;
+            // Checking not out of boundary
+            if (leftIdx < len) {
+                leftChild = this.values[leftIdx];
+                if (leftChild > el) {
+                    swap = leftIdx;
+                }
+            }
+
+            if (rightIdx < len) {
+                rightChild = this.values[rightIdx];
+                // Checking if left is swap or not
+                if ((!swap && rightChild > el) || (swap && rightChild > leftChild)) {
+                    swap = rightIdx;
+                }
+            }
+
+            if (!swap) break;
+
+            this.values[idx] = this.values[swap];
+            this.values[swap] = el;
+
+            idx = swap;
+        }
+    }
+
+    extract() {
+        const max = this.values[0];
+        const end = this.values.pop();
+
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+
+        return max;
+    }
 }
 
 let heap = new MaxBinaryHeap();
 heap.insert(55);
+heap.insert(39);
+heap.insert(41);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
 heap.insert(33);
-heap.insert(99);
-heap.insert(9);
-heap.insert(36);
-heap.insert(21);
+console.log(heap.values);
+heap.extract();
+heap.extract();
+console.log(heap.values);
+heap.extract();
+heap.extract();
+heap.extract();
+console.log(heap.values);
+heap.extract();
+heap.extract();
+heap.extract();
 console.log(heap.values);
